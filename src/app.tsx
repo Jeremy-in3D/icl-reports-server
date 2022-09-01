9df9d364a03b15ce5edc229d00988a6dfa748bb8
Eliya Vahl <eliya@in3d-tech.com>
2022-09-01 16:27:50 +0300

    build: added icons

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,30 @@
 import React from "react";
 
+const notificationReportIcon = new URL(
+  "../assets/icons/report-notification.svg",
+  import.meta.url
+);
+const searchReportIcon = new URL(
+  "../assets/icons/report-search.svg",
+  import.meta.url
+);
+const addReportIcon = new URL(
+  "../assets/icons/report-add.svg",
+  import.meta.url
+);
+const shareReportIcon = new URL(
+  "../assets/icons/report-share.svg",
+  import.meta.url
+);
+const settingsReportIcon = new URL(
+  "../assets/icons/report-settings.svg",
+  import.meta.url
+);
+const statisticsReportIcon = new URL(
+  "../assets/icons/report-statistics.svg",
+  import.meta.url
+);
+
 export const App: React.FC = () => {
   return (
     <div className="app">
@@ -12,12 +37,36 @@ export const App: React.FC = () => {
           <div className="user item">אליה והל</div>
         </div>
         <div className="main-menu">
-          <div className="block-item">דו"ח מכלולים</div>
-          <div className="block-item">דו"ח שמנים</div>
-          <div className="block-item">דו"ח רעידות</div>
-          <div className="block-item">מסך התראות</div>
-          <div className="block-item">ייצוא</div>
-          <div className="block-item">סטטיסטיקה</div>
+          <div className="block-item">
+            <img className="main-menu-icon" src={addReportIcon.href}></img>
+            <p>דו"ח מכלולים</p>
+          </div>
+          <div className="block-item">
+            <img className="main-menu-icon" src={searchReportIcon.href}></img>
+            <p>חיפוש דוחות</p>
+          </div>
+          <div className="block-item">
+            <img
+              className="main-menu-icon"
+              src={notificationReportIcon.href}
+            ></img>
+            <p>מסך התראות</p>
+          </div>
+          <div className="block-item">
+            <img className="main-menu-icon" src={shareReportIcon.href}></img>
+            <p>ייצוא</p>
+          </div>
+          <div className="block-item">
+            <img
+              className="main-menu-icon"
+              src={statisticsReportIcon.href}
+            ></img>
+            <p>סטטיסטיקה</p>
+          </div>
+          <div className="block-item">
+            <img className="main-menu-icon" src={settingsReportIcon.href}></img>
+            <p>הגדרת דוחות</p>
+          </div>
         </div>
       </div>
     </div>
