a380d7fea635ada85f1fdabf99372986ec041004
Eliya Vahl <eliya@in3d-tech.com>
2022-09-04 19:17:44 +0300

    build: app styling and reports initial layout

--- src/components/reports.tsx
+++ src/components/reports.tsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { TakePicture } from "./take-picture";
 
 export const Reports: React.FC = () => {
-  return <div className="report-screen"></div>;
+  return (
+    <div className="reports">
+      <button className="report-button">סיור ראשון - א</button>
+      <button className="report-button">סיור שני - ב</button>
+      <button className="report-button">סיור שלישי - ג</button>
+      <button className="report-button">סיור רביעי - ד</button>
+      <button className="report-button">סיור חמישי - ה</button>
+    </div>
+  );
 };
