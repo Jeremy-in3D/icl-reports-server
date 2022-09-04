8795bc3b96b10dcf271a7838cefb1bb66366de0a
Eliya Vahl <eliya@in3d-tech.com>
2022-09-04 16:46:13 +0300

    build: take photo logic

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
+const fs = require("fs");
+const https = require("https");
 const express = require("express");
 const path = require("path");
 
+const privateKey = fs.readFileSync("certs/server.key", "utf8");
+const certificate = fs.readFileSync("certs/server.crt", "utf8");
+const credentials = { key: privateKey, cert: certificate };
 const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.listen(port, () => {
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+// const httpsServer = https.createServer(credentials, app);
+// httpsServer.listen(port, () => {
+//   console.log(`Server is listening on port ${port}`);
+// });
 
 app.use(express.static(path.join(__dirname, "dist")));
 
--- src/components/reports.tsx
+++ src/components/reports.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Reports: React.FC = () => {
-  return <div>Reports Screen</div>;
+  return <div className="report-screen"></div>;
 };
