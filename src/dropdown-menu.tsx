361d7873fc71376b4caac6ee84471ebdf3295ab5
Eliya Vahl <eliya@in3d-tech.com>
2022-09-04 12:09:42 +0300

    fix: updated menu styling

--- src/dropdown-menu.tsx
+++ src/dropdown-menu.tsx
@@ -4,20 +4,23 @@ export const DropdownMenu: React.FC = () => {
   const [isOpened, setIsOpened] = useState(false);
 
   return (
-    <div className={`dropdown-menu ${isOpened && "opened"}`}>
+    <div className={`dropdown-menu ${isOpened ? "opened" : "closed"}`}>
       {isOpened ? (
         <div>
           <p className="title">מארכת דוחות</p>
-          <div className="item">כפתור</div>
-          <div className="item">כפתור</div>
-          <div className="item">כפתור</div>
-          <div className="user item">אליה והל</div>
+          <div className="item">מסך בית</div>
+          <div className="item">התחברות \ החלף משתמש</div>
+          <div className="item">הגדרות</div>
+          <div className="user">אליה והל</div>
           <div className="toggle-dropdown" onClick={() => setIsOpened(false)}>
             ▲
           </div>
         </div>
       ) : (
-        <div onClick={() => setIsOpened(true)} className="dropdown-menu">
+        <div
+          onClick={() => setIsOpened(true)}
+          className="toggle-dropdown closed"
+        >
           ▼
         </div>
       )}
