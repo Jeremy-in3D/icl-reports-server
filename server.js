135bc6b247722e9dd12e87a85c20a1d58df1becc
Eliya Vahl <eliya@in3d-tech.com>
2022-11-02 12:20:49 +0200

    fix: access to azure blob storage

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 require("dotenv").config();
-const fs = require("fs");
+// const fs = require("fs");
 const http = require("http");
-// const https = require("https");
 const express = require("express");
 const path = require("path");
-const MongoDB = require("./mongodb.js");
-const XLSX = require("xlsx");
+const { BlobServiceClient } = require("@azure/storage-blob");
 const sampleData = require("./assets/test-data.json");
-
+const MongoDB = require("./mongodb.js");
 const mongo = new MongoDB(process.env.MONGO_URI);
 
-const privateKey = fs.readFileSync("certs/server.key", "utf8");
-const certificate = fs.readFileSync("certs/server.crt", "utf8");
-const credentials = { key: privateKey, cert: certificate };
 const port = process.env.PORT || 8080;
-
 const app = express();
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.json());
@@ -52,27 +46,34 @@ app.post("/save-report", async (req, res) => {
   }
 });
 
+const BLOB_CONNECTION_STRING = process.env.BLOB_CONNECTION_STRING || "";
+// Note - Account connection string can only be used in node.
+const blobServiceClient = BlobServiceClient.fromConnectionString(
+  BLOB_CONNECTION_STRING
+);
+
 (async () => {
   try {
-    await mongo.connect();
+    // await mongo.connect();
+    let i = 1;
+    for await (const container of blobServiceClient.listContainers()) {
+      console.log(`Container ${i++}: ${container.name}`);
+    }
     const httpServer = http.createServer(app);
     httpServer.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
     });
-    httpServer.addListener("close", () => mongo.close());
   } catch (e) {
     console.log("Error", e);
   }
 })();
 
-//Close the MongoDB connection on process close
-process.on("SIGINT", () => {
-  mongo.close();
-  console.log("Process Closed");
-});
 //Add connection referesh for mongodb
-
 //---------------------HTTPS
+// const privateKey = fs.readFileSync("certs/server.key", "utf8");
+// const certificate = fs.readFileSync("certs/server.crt", "utf8");
+// const credentials = { key: privateKey, cert: certificate };
+
 // const httpsServer = https.createServer(credentials, app);
 // httpsServer.listen(port, () => {
 //   console.log(`Server is listening on port ${port}`);
