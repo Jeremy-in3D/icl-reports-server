3865a6ea83e0449ddd612b2d6b240243cf10421d
Eliya Vahl <eliya@in3d-tech.com>
2022-12-04 16:26:09 +0200

    data: add route 1 questions and data

--- client/src/data/machine-parts.ts
+++ client/src/data/machine-parts.ts
@@ -1,7 +1,7 @@
 export const machineParts: MachineParts = [
   {
-    id: "1",
-    name: "משפך קבלה",
+    questionId: "Q01",
+    partName: "צנרת וברזים",
     type: "checkboxes",
     input: [
       {
@@ -9,20 +9,134 @@ export const machineParts: MachineParts = [
         options: false,
         alert: false,
       },
-      { text: "קיימים נזילות ממעטפת המשפך", options: false, alert: false },
       {
-        text: "קיימים מגוני צד מפורקים לאורך הסרט",
-        alert: false,
+        text: "מחבר גמיש של המשאבה יבש או קרוע",
         options: true,
-        choices: ["הנעה", "ראש", "משקולת", "הידוק", "הטייה"],
+        alert: false,
+        choices: ["יניקה", "סניקה"],
       },
-      { text: "קיימים מרעדים לא תקינים", options: false, alert: false },
-      { text: " מצב הסקרטינג נדרשת הנמכה/החלפה", options: false, alert: false },
+      {
+        text: "פילטר המשאבה נוזל",
+        alert: false,
+        options: false,
+      },
+      {
+        text: "מחבר גמיש של המשאבה יבש או קרוע",
+        options: true,
+        alert: false,
+        choices: ["יניקה", "סניקה"],
+      },
+      {
+        text: "תמיכות צנרת המשאבה או הפילטר לא תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "בוחש המיכל לא מסתובב/נפל למיכל/רועד",
+        options: false,
+        alert: true,
+      },
+      {
+        text: "קיימת נזילה/הזעה מהמכילת ומהאוגנים שלו",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
     ],
   },
   {
-    id: "2",
-    name: "מנוע",
+    questionId: "Q02",
+    partName: "מנוע",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "מנוע המסוע חם",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "קיים חוסר של פירמוט או פירמוט ריקות במנוע",
+        alert: false,
+        options: false,
+      },
+      {
+        text: "קיימים רעשים חריגים מהמנוע",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "יש צורך בניקיון צלעות מנוע ",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "אומגה של המשאבה סדוקה או יבשה",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "קיימים ברגים משוחררים בבסיס המנוע או במשאבה",
+        options: false,
+        alert: true,
+      },
+    ],
+  },
+  {
+    questionId: "Q03",
+    partName: "משאבה",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "מסבי המשאבה חמים",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "קיימים רעשים/רעידות חריגים במשאבה",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "קיימת נזילת מים מאטם המשאבה/גוף המשאבה",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "חסרות פרמוט/שמן במשאבה או לא קיים",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "מיגון המשאבה או האטם  לא תקין",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "צנרת מי האטימה לא מחובר לא פתוח",
+        options: false,
+        alert: true,
+      },
+    ],
+  },
+  {
+    questionId: "Q04",
+    partName: "מנוע",
     type: "checkboxes",
     input: [
       {
@@ -48,6 +162,165 @@ export const machineParts: MachineParts = [
         choices: ["גבולי", "חריג", "קריטי"],
       },
       {
+        text: "יש צורך בניקיון צלעות מנוע ",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קימיים רעשים חריגים מהמצמד המקשר",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "פירמוט הגירוז במנוע חוסרות/ריקות",
+        options: false,
+        alert: false,
+      },
+    ],
+  },
+  {
+    questionId: "Q05",
+    partName: "ציר ומסבי המפוח",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קיימים רעשים חריגים ממסבי המפוח",
+        options: false,
+        alert: true,
+      },
+      {
+        text: "קיים חוסר שמן/פירמוט גירוז במסבי המפוח",
+        alert: true,
+        options: false,
+      },
+      {
+        text: "קיימים רעידות חריגות במפוח ובסביבתו",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "מיגון לבטח רוקד או אינו מוחבר",
+        options: false,
+        alert: true,
+      },
+      {
+        text: "גשש הרעידות או הטמפ' אינם מחוברים או כבל פגוע",
+        options: false,
+        alert: false,
+      },
+    ],
+  },
+  {
+    questionId: "Q06",
+    partName: "בית המפוח",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קיים חורים/נזילות בבית המפוח או במחברי המפוח",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קיימים ברגים חסרים/משוחררים במפוח",
+        alert: false,
+        options: false,
+      },
+      {
+        text: "קיימים רעידות חריגות במפוח ובסביבתו",
+        options: false,
+        alert: true,
+      },
+      {
+        text: "קיימים סדקים או ברגים משוחררים בבסיס המפוח",
+        options: false,
+        alert: true,
+      },
+    ],
+  },
+  {
+    questionId: "Q07",
+    partName: "משפך קבלה",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קיימים נזילות ממעטפת המשפך",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קיימים מגוני צד מפורקים לאורך הסרט",
+        alert: false,
+        options: true,
+        choices: ["הנעה", "ראש", "משקולת", "הידוק", "הטייה"],
+      },
+      {
+        text: "קיימים מרעדים לא תקינים",
+        options: false,
+        alert: false,
+      },
+      {
+        text: " מצב הסקרטינג נדרשת הנמכה/החלפה",
+        options: false,
+        alert: false,
+      },
+    ],
+  },
+  {
+    questionId: "Q08",
+    partName: "מנוע",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "מנוע המסוע חם",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "מכסה המאוור או מאוורר המנוע שבורים",
+        alert: false,
+        options: false,
+      },
+      {
+        text: "קיימים רעשים חריגים מהמנוע",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "יש צורך בניקיון צלעות מנוע",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קימיים רעשים חריגים מהמצמד המקשר לממסרה",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
         text: "קיימים ברגים משוחררים בבסיס המנוע או הממסרה",
         options: false,
         alert: true,
@@ -55,8 +328,8 @@ export const machineParts: MachineParts = [
     ],
   },
   {
-    id: "3",
-    name: "ממסרה",
+    questionId: "Q09",
+    partName: "ממסרה",
     type: "checkboxes",
     input: [
       {
@@ -64,20 +337,45 @@ export const machineParts: MachineParts = [
         options: false,
         alert: false,
       },
-      { text: "קיימים נזילות ממעטפת המשפך ", options: false, alert: false },
       {
-        text: "קיימים מגוני צד מפורקים לאורך הסרט",
-        alert: false,
+        text: "ממסרה של המסוע חמה",
         options: true,
-        choices: ["הנעה", "ראש", "משקולת", "הידוק", "הטייה"],
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
       },
-      { text: "קיימים מרעדים לא תקינים", options: false, alert: false },
-      { text: " מצב הסקרטינג נדרשת הנמכה/החלפה", options: false, alert: false },
+      {
+        text: "קיימים רעשים חריגים מהממסרה",
+        options: true,
+        alert: false,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "קיימת נזילת שמן מהממסרה",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "גובה שמן הממסרה לא תקין או לא ניתן לראות",
+        options: false,
+        alert: true,
+      },
+      {
+        text: "מיגון במערכת ההנעה לא תקין",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "מצמד המקשר לתוף הנעה מרעיש",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
     ],
   },
   {
-    id: "4",
-    name: "סרט",
+    questionId: "Q10",
+    partName: "סרט",
     type: "checkboxes",
     input: [
       {
@@ -85,20 +383,39 @@ export const machineParts: MachineParts = [
         options: false,
         alert: false,
       },
-      { text: "קיימים נזילות ממעטפת המשפך ", options: false, alert: false },
       {
-        text: "קיימים מגוני צד מפורקים לאורך הסרט",
-        alert: false,
+        text: "חיבורי המסוע קלמרות/הדבקה נפתחו",
         options: true,
-        choices: ["הנעה", "ראש", "משקולת", "הידוק", "הטייה"],
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
       },
-      { text: "קיימים מרעדים לא תקינים", options: false, alert: false },
-      { text: " מצב הסקרטינג נדרשת הנמכה/החלפה", options: false, alert: false },
+      {
+        text: "המסוע סוטה",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "המסוע שחוק",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "המסוע קרוע או קיימים חתכים לאורכו",
+        options: false,
+        alert: true,
+      },
+      {
+        text: "המסוע רפוי/מתוח מידיי",
+        options: false,
+        alert: false,
+      },
     ],
   },
   {
-    id: "5",
-    name: "תופים",
+    questionId: "Q11",
+    partName: "תופים",
     type: "checkboxes",
     input: [
       {
@@ -106,30 +423,277 @@ export const machineParts: MachineParts = [
         options: false,
         alert: false,
       },
-      { text: "קיימים נזילות ממעטפת המשפך ", options: false, alert: false },
       {
-        text: "קיימים מגוני צד מפורקים לאורך הסרט",
-        alert: false,
+        text: "ציפוי הגומי בתופי המסוע לא תקינה",
         options: true,
+        alert: false,
         choices: ["הנעה", "ראש", "משקולת", "הידוק", "הטייה"],
       },
-      { text: "קיימים מרעדים לא תקינים", options: false, alert: false },
-      { text: " מצב הסקרטינג נדרשת הנמכה/החלפה", options: false, alert: false },
+      {
+        text: "קיים חוסר במערכת גירוז למסבי התוף",
+        options: true,
+        alert: false,
+        choices: ["הנעה", "ראש", "משקולת", "הידוק", "הטייה"],
+      },
+      {
+        text: "קיימים רעשים חריגים מהתוף או ממסבב",
+        options: true,
+        alert: true,
+        choices: ["הנעה", "ראש", "משקולת", "הידוק", "הטייה"],
+      },
+      {
+        text: "קיימת תזוזה של התוף בבתי המיסב",
+        options: true,
+        alert: true,
+        choices: ["הנעה", "ראש", "משקולת", "הידוק", "הטייה"],
+      },
     ],
   },
-
-  // {
-  //   id: "question2",
-  //   type: "range",
-  //   question: "מאחד את עשר כמה אוהב חיות?",
-  //   options: { start: 0, end: 10, step: 1 },
-  // },
-  // {
-  //   id: "question3",
-  //   type: "text",
-  //   question: "מה על הראש שלך?",
-  //   options: null,
-  // },
+  {
+    questionId: "Q12",
+    partName: "גלילים",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קיימים גלילים לא תקינים במסוע",
+        options: true,
+        alert: false,
+        choices: ["שוקת", "חוזרים", "נושאים", "כיוון חוזר", "נושא חוזר"],
+      },
+      {
+        text: "קימיים גלילים בנויים בחומר לאורך המנוע",
+        options: true,
+        alert: false,
+        choices: ["שוקת", "חוזרים", "נושאים", "כיוון חוזר", "נושא חוזר"],
+      },
+      {
+        text: "קיים גליל מרעיש לאורך הסרט",
+        options: true,
+        alert: true,
+        choices: ["שוקת", "חוזרים", "נושאים", "כיוון חוזר", "נושא חוזר"],
+      },
+    ],
+  },
+  {
+    questionId: "Q13",
+    partName: "מערכות נלוות למסוע",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "מגרדות המסוע דורשות טיפול וקיים זרזוף בסרט החוזר",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "קונסטרוקצית המסוע פגוע",
+        options: true,
+        alert: false,
+        choices: ["שוקת", "חוזרים", "נושאים", "כיוון חוזר", "נושא חוזר"],
+      },
+      {
+        text: "יש צורך בניקיון בסביבת הסרט",
+        options: true,
+        alert: false,
+        choices: ["שוקת", "חוזרים", "נושאים", "כיוון חוזר", "נושא חוזר"],
+      },
+      {
+        text: "כבל/גלגלת/מבנה מערכת המתחיה פגום",
+        options: false,
+        alert: true,
+      },
+      {
+        text: "גלגל משקלת במסוע משוחרר/עמדת המשקל עקומה ולא תקינה",
+        options: false,
+        alert: false,
+      },
+    ],
+  },
+  {
+    questionId: "Q14",
+    partName: "טריפר",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "מערכת הטריפר בהטייה",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קיימת בניה במשפך הטריפר והסרט נוסע על חומר",
+        options: false,
+        alert: false,
+      },
+    ],
+  },
+  {
+    questionId: "Q15",
+    partName: "משפך קבלה וגוף החילזון",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קיימים נזילות/בריחת ואקום ממעטפת החילזון או המשפך",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קיימים מגוני RD מפורק או לא יושב טוב",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קיימים רעשים חריגים מכיוון החלזון",
+        options: false,
+        alert: false,
+      },
+    ],
+  },
+  {
+    questionId: "Q16",
+    partName: "מנוע",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "מנוע המסוע חם",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "מכסה המאוור או מאוורר המנוע שבורים",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קיימים רעשים חריגים מהמנוע",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "יש צורך בניקיון צלעות מנוע",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קימיים רעשים חריגים מהמצמד המקשר לממסרה",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "קיימים ברגים משוחררים בבסיס המנוע או הממסרה",
+        options: false,
+        alert: true,
+      },
+    ],
+  },
+  {
+    questionId: "Q17",
+    partName: "ממסרה",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "ממסרה של החילזון חמה",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "קיימים רעשים חריגים מהממסרה",
+        options: true,
+        alert: false,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "קיימת נזילת שמן מהממסרה",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+      {
+        text: "גובה שמן הממסרה לא תקין או לא ניתן לראות",
+        options: false,
+        alert: true,
+      },
+      {
+        text: "מיגון במערכת ההנעה לא תקין",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "מצמד המקשר בין ההנעה לחילזון מרעיש/לא תקין",
+        options: true,
+        alert: true,
+        choices: ["גבולי", "חריג", "קריטי"],
+      },
+    ],
+  },
+  {
+    questionId: "Q18",
+    partName: "חילזון",
+    type: "checkboxes",
+    input: [
+      {
+        text: "כלל הערות למטה תקינות",
+        options: false,
+        alert: false,
+      },
+      {
+        text: "קיים מיסב חם/מרעיש בחילזון",
+        options: true,
+        alert: true,
+        choices: ["הנעה", "זנב"],
+      },
+      {
+        text: "לא קיים גירוז במיסב החילזון",
+        options: true,
+        alert: true,
+        choices: ["הנעה", "זנב"],
+      },
+      {
+        text: "קיימת נזילה מאטם החילזון",
+        options: true,
+        alert: true,
+        choices: ["הנעה", "זנב"],
+      },
+      {
+        text: "קיימים רעידות בגוף החילזון אוברגים משוחריים במסבים",
+        options: false,
+        alert: true,
+      },
+    ],
+  },
 ];
 
 export type CheckBox =
@@ -137,8 +701,8 @@ export type CheckBox =
   | { text: string; alert: boolean; options: false };
 
 export type MachineParts = {
-  id: string;
-  name: string;
+  questionId: string;
+  partName: string;
   type: "checkboxes" | "multiple-choice" | "range";
   input: CheckBox[];
 }[];
--- client/src/data/reports-data.ts
+++ client/src/data/reports-data.ts
@@ -1,21 +1,120 @@
 export const michlolim: MichlolContents[] = [
   {
     michlolId: "R1-M1",
-    michlolName: "מסוע משקולת",
+    michlolName: "משאבה",
+    equipmentUnit: "משאבת סחרור משטף אבק, קו מייבש",
     machines: {
-      "R-2010-050-209": ["1", "2", "3", "4", "5"],
-      "R-2010-050-207": ["1", "2"],
+      "R-2010-050-454": ["Q01", "Q02", "Q03"],
+      "R-2010-050-455": ["Q01", "Q02", "Q03"],
+      "R-2010-050-458": ["Q01", "Q02", "Q03"],
+      "R-2010-050-459": ["Q01", "Q02", "Q03"],
+      "R-2010-050-461": ["Q01", "Q02", "Q03"],
+      "R-2010-050-462": ["Q01", "Q02", "Q03"],
     },
   },
   {
     michlolId: "R1-M2",
-    michlolName: "מגרסה",
-    machines: { "R-2010-050-205": ["1"] },
+    michlolName: "משאבה",
+    equipmentUnit: "משאבת סחרור משטף אבק, קו מקרר",
+    machines: {
+      "R-2010-050-464": ["Q01", "Q02", "Q03"],
+      "R-2010-050-468": ["Q01", "Q02", "Q03"],
+      "R-2010-050-469": ["Q01", "Q02", "Q03"],
+      "R-2010-050-471": ["Q01", "Q02", "Q03"],
+      "R-2010-050-472": ["Q01", "Q02", "Q03"],
+      "R-2010-050-491": ["Q01", "Q02", "Q03"],
+    },
   },
   {
     michlolId: "R1-M3",
     michlolName: "משאבה",
-    machines: { "R-2010-050-454": ["1"] },
+    equipmentUnit: "משאבת מים לגרעון",
+    machines: {
+      "R-2010-050-479": ["Q01", "Q02", "Q03"],
+      "R-2010-050-480": ["Q01", "Q02", "Q03"],
+    },
+  },
+  {
+    michlolId: "R1-M4",
+    michlolName: "משאבה",
+    equipmentUnit: "משאבת שפכים",
+    machines: {
+      "R-2010-050-484": ["Q01", "Q02", "Q03"],
+      "R-2010-050-485": ["Q01", "Q02", "Q03"],
+    },
+  },
+  {
+    michlolId: "R1-M5",
+    michlolName: "משאבה בור",
+    equipmentUnit: "משאבת בור ניקוזים ספיגה",
+    machines: {
+      "R-2010-050-486": ["Q01", "Q02", "Q03"],
+    },
+  },
+  {
+    michlolId: "R1-M6",
+    michlolName: "מפוח",
+    equipmentUnit: "מפוח מקרר",
+    machines: {
+      "R-2010-050-453": ["Q04", "Q05", "Q06"],
+    },
+  },
+  {
+    michlolId: "R1-M7",
+    michlolName: "מפוח",
+    equipmentUnit: "מפוח מייבש",
+    machines: { "R-2010-050-452": ["Q04", "Q05", "Q06"] },
+  },
+  {
+    michlolId: "R1-M8",
+    michlolName: "מסוע משקולת",
+    equipmentUnit: "מסוע תוצרת",
+    machines: {
+      "R-2010-050-052": ["Q07", "Q08", "Q09", "Q10", "Q11", "Q12", "Q13"],
+      "R-2010-050-054": ["Q07", "Q08", "Q09", "Q10", "Q11", "Q12", "Q13"],
+    },
+  },
+  {
+    michlolId: "R1-M9",
+    michlolName: "מסוע טריפר",
+    equipmentUnit: "מסוע טריפר",
+    machines: {
+      "R-2010-050-057": [
+        "Q07",
+        "Q08",
+        "Q09",
+        "Q10",
+        "Q11",
+        "Q12",
+        "Q13",
+        "Q14",
+      ],
+    },
+  },
+  {
+    michlolId: "R1-M10",
+    michlolName: "מסוע משקולת",
+    equipmentUnit: "מסוע מפזר",
+    machines: {
+      "R-2010-050-100": ["Q07", "Q08", "Q09", "Q10", "Q11", "Q12", "Q13"],
+      "R-2010-050-400": ["Q07", "Q08", "Q09", "Q10", "Q11", "Q12", "Q13"],
+    },
+  },
+  {
+    michlolId: "R1-M11",
+    michlolName: "חילזון",
+    equipmentUnit: "חילזון מקרר",
+    machines: {
+      "R-2010-050-075": ["Q15", "Q16", "Q17", "Q18"],
+    },
+  },
+  {
+    michlolId: "R1-M12",
+    michlolName: "חילזון",
+    equipmentUnit: "חילזון מייבש",
+    machines: {
+      "R-2010-050-074": ["Q15", "Q16", "Q17", "Q18"],
+    },
   },
 ];
 
@@ -23,17 +122,31 @@ export const routes: Routes = [
   {
     routeId: "R1",
     routeName: "מסלול מספר 1",
-    michlolim: ["R1-M1", "R1-M2", "R1-M3"],
+    michlolim: [
+      "R1-M1",
+      "R1-M2",
+      "R1-M3",
+      "R1-M4",
+      "R1-M5",
+      "R1-M6",
+      "R1-M7",
+      "R1-M8",
+      "R1-M9",
+      "R1-M10",
+      "R1-M11",
+      "R1-M12",
+    ],
   },
   {
     routeId: "R2",
     routeName: "מסלול מספר 2",
-    michlolim: ["R1-M1", "R1-M2", "R1-M3"],
+    michlolim: ["R2-M1", "R2-M2", "R2-M3"],
   },
 ];
 export type MichlolContents = {
   michlolId: string;
   michlolName: string;
+  equipmentUnit: string;
   machines: { [id: string]: string[] };
 };
 
