194f0ccf1ea84ff94c4ce601dbe067ac2682b4e2
Eliya Vahl <eliya@in3d-tech.com>
2023-03-05 15:55:18 +0200

    server-to-match-client

--- server.ts
+++ server.ts
@@ -42,6 +42,18 @@ app.use(
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.json());
 app.use(express.text());
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://icl-report-client.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 
 //Blob initilization for images saving in Azure
